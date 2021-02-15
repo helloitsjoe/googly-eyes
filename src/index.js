81aa380a1b8fc07f34f2e61167f8cde0488d27ca
helloitsjoe <jboyle617@gmail.com>
2021-02-14 22:06:09 -0500

    DeviceOrientation test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const stats = document.getElementById('stats');
+const media = document.getElementById('media');
+
 window.addEventListener('drop', e => {
   e.preventDefault();
   console.log(e.dataTransfer.files[0]);
@@ -20,3 +23,58 @@ window.addEventListener('dragstart', e => {
 window.addEventListener('dragover', e => {
   e.preventDefault();
 });
+
+window.requestPermission = () => {
+  if (typeof DeviceOrientationEvent !== 'undefined') {
+    stats.textContent = `DeviceOrientation: ${Object.keys(
+      DeviceOrientationEvent
+    )}`;
+    DeviceOrientationEvent.requestPermission()
+      .then(permissionState => {
+        stats.textContent = `permission: ${permissionState}`;
+        if (permissionState === 'granted') {
+          window.addEventListener('deviceorientation', e => {
+            stats.textContent = `orientation: ${e}`;
+            const { absolute, alpha, beta, gamma } = e;
+            console.log(`absolute:`, absolute);
+            console.log(`alpha:`, alpha);
+            console.log(`beta:`, beta);
+            console.log(`gamma:`, gamma);
+            stats.textContent = JSON.stringify(
+              { absolute, alpha, beta, gamma },
+              null,
+              2
+            );
+          });
+
+          window.addEventListener('devicemotion', e => {
+            stats.textContent = `motion: ${e}`;
+            const { absolute, alpha, beta, gamma } = e;
+            console.log(`absolute:`, absolute);
+            console.log(`alpha:`, alpha);
+            console.log(`beta:`, beta);
+            console.log(`gamma:`, gamma);
+            stats.textContent = JSON.stringify(
+              { absolute, alpha, beta, gamma },
+              null,
+              2
+            );
+          });
+        }
+      })
+      .catch(console.error);
+  } else {
+    stats.textContent = 'DeviceOrientationEvent is undefined';
+  }
+};
+// const getUserMedia = navigator;
+// document.getElementById('media').textContent = `getUserMedia: ${getUserMedia}`;
+// if (getUserMedia) {
+//   console.log(`getUserMedia:`, getUserMedia);
+//   getUserMedia({ photo: true }).then(stream => {
+//     console.log(stream);
+//     document.getElementById('media').textContent = stream;
+//   });
+// } else {
+//   console.log('getUserMedia is not defined');
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 /* eslint-env node */
 const path = require('path');
+const ip = require('ip');
 const { makeWebpackConfig } = require('webpack-simple');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = makeWebpackConfig();
 
+config.output = {
+  path: path.join(process.cwd(), 'docs'),
+};
 config.module = {};
 
 config.devServer = {
   contentBase: path.join(__dirname, 'dist'),
   compress: true,
+  https: true,
+  // host: '0.0.0.0',
   port: 9000,
 };
 
@@ -17,4 +23,5 @@ config.plugins = [
   new HtmlWebpackPlugin({ template: 'index.template.html', output: 'dist' }),
 ];
 
+console.log(`Running on ${ip.address()}:9000`);
 module.exports = config;
